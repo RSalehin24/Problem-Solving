e2b55c56eedc8958eebbf1e0a8ce306ba70ed7e1
Md. Reyanus Salehin <reyanussalehin@iut-dhaka.edu>
2020-12-23 13:59:09 +0600

    Create arraypointer.cpp

---
+++ arraypointer.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    
+    int n, q, k;
+    cin >> n >> q;
+    
+    int** arr = new int*[n];
+    
+    for(int i=0; i<n; i++){
+        cin >> k;
+        arr[i] = new int[k];
+        
+        for(int j=0; j<k; j++){
+            cin >> arr[i][j];
+        }
+    }
+    
+    for(int i=0; i<q; i++){
+        int q1, q2;
+        cin >> q1 >> q2;
+        cout << arr[q1][q2] <<endl;
+    }
+    
+    return 0;
+}
